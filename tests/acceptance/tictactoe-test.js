3e11b63fff43ee1a46653d6c01c5b29edc0a219f
Eaylcn <82917052+Eaylcn@users.noreply.github.com>
2022-12-22 15:45:42 +0300

    Add files via upload
    New tests added.

--- tests/acceptance/tictactoe-test.js
+++ tests/acceptance/tictactoe-test.js
@@ -5,8 +5,6 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | tictactoe', function (hooks) {
   setupApplicationTest(hooks);
 
-  const emptyCell1 = assert.equal(getRootElement(), document.querySelector("body > div.content > div > div:nth-child(1)"));
-  const xCell1 = assert.equal(getRootElement(), document.querySelector("body > div.content > div > div.cell.x"));
 
   test('The user can access other pages from the homepage', async function (assert) {
     await visit('/');
@@ -31,11 +29,345 @@ module('Acceptance | tictactoe', function (hooks) {
   test('The user can play a game', async function (assert) {
     await visit('/game');
     assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
-    assert.dom('data-test-game--grid').exists('it renders cells');
-    await click(emptyCell1);
-    assert.dom(xCell1).exists('game has started');
+    assert.dom('[data-test-cell-id="0"]').exists('it renders cells');
+    await click('[data-test-cell-id="0"]');
+    assert.dom('[data-test-cell-status="x"]').exists('game has started');
+  });
 
-    // Add your tests here!
-    assert.strictEqual('Good luck writing tests!', 'Good luck writing tests!');
+  test('while playing click instructions', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-menu-right] a');
+    assert.strictEqual(currentURL(), '/how-to-play', "its instructions page");
+    assert.dom('ol li').exists('it renders instructions');
+  });
+
+  test('while playing click logo', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-menu-center] a');
+    assert.strictEqual(currentURL(), '/', "its homepage");
+  });
+
+  test('while playing click restart', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+  });
+
+  test('click logo after o win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="6"]');
+    assert.dom('[data-test-game--status="o"]').exists('first column o win');
+    await click('[data-test-menu-center] a');
+    assert.strictEqual(currentURL(), '/', "its homepage");
+  });
+
+  test('click logo after x win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="6"]');
+    assert.dom('[data-test-game--status="x"]').exists('first column x win');
+    await click('[data-test-menu-center] a');
+    assert.strictEqual(currentURL(), '/', "its homepage");
+  });
+
+  test('click logo after draw', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="7"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+    await click('[data-test-menu-center] a');
+    assert.strictEqual(currentURL(), '/', "its homepage");
+  });
+
+  test('click instructions after o win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="7"]');
+    assert.dom('[data-test-game--status="o"]').exists('second column o win');
+    await click('[data-test-menu-right] a');
+    assert.strictEqual(currentURL(), '/how-to-play', "its instructions page");
+  });
+
+  test('click instructions after x win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="7"]');
+    assert.dom('[data-test-game--status="x"]').exists('second column x win');
+    await click('[data-test-menu-right] a');
+    assert.strictEqual(currentURL(), '/how-to-play', "its instructions page");
+  });
+
+  test('click instructions after draw', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+    await click('[data-test-menu-right] a');
+    assert.strictEqual(currentURL(), '/how-to-play', "its instructions page");
+  });
+
+  test('x win after o win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="o"]').exists('third column o win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="x"]').exists('third column x win');
+  });
+
+  test('x win after x win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="x"]').exists('0-8 diagonal x win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="6"]');
+    assert.dom('[data-test-game--status="x"]').exists('2 - 6 diagonal x win');
+  });
+
+  test('x win after draw', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="0"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="2"]');
+    assert.dom('[data-test-game--status="x"]').exists('first row x win');
+  });
+
+  test('o win after x win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="5"]');
+    assert.dom('[data-test-game--status="x"]').exists('second row x win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="2"]');
+    assert.dom('[data-test-game--status="o"]').exists('first row o win');
+  });
+
+  test('o win after o win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="o"]').exists('0-8 diagonal o win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="6"]');
+    assert.dom('[data-test-game--status="o"]').exists('2 - 6 diagonal o win');
+  });
+
+  test('o win after draw', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="5"]');
+    assert.dom('[data-test-game--status="o"]').exists('second row o win');
+  });
+
+  test('draw after draw', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="6"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+  });
+
+  test('draw after o win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="o"]').exists('third row o win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="8"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="3"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
+  });
+
+  test('draw after x win', async function (assert) {
+    await visit('/game');
+    assert.strictEqual(currentURL(), '/game', "it doesn't redirect");
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--status="x"]').exists('third row x win');
+    await click('[data-test-button-restart]');
+    assert.dom('[data-test-cell-status="x"]').doesNotExist('x isnt on screen');
+    assert.dom('[data-test-cell-status="o"]').doesNotExist('o isnt on screen');
+    assert.strictEqual(currentURL(), '/game', "game restarted");
+    await click('[data-test-cell-id="2"]');
+    await click('[data-test-cell-id="0"]');
+    await click('[data-test-cell-id="4"]');
+    await click('[data-test-cell-id="1"]');
+    await click('[data-test-cell-id="3"]');
+    await click('[data-test-cell-id="5"]');
+    await click('[data-test-cell-id="7"]');
+    await click('[data-test-cell-id="6"]');
+    await click('[data-test-cell-id="8"]');
+    assert.dom('[data-test-game--grid-active="inactive"]').exists('game is draw and inactive');
   });
 });
